5970e938b53ecf0f9b95b737281b0d78614458b5
Devansh Sharma <debu1@gmail.com>
2025-08-08 22:09:37 +0530

    disable prerendering for dynamic routes

--- smart-job-portal/frontend/src/app/app.routes.server.ts
+++ smart-job-portal/frontend/src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '/login',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/register',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '/dashboard/recruiter/manage-applications/:jobId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
